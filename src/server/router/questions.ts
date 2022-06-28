53e8b188e61b18e5317d3ba7c070016dc0c0b606
Kutay Sakallioglu <kutay@vestiyer.co>
2022-06-28 17:10:54 +0300

    question mutation

--- src/server/router/questions.ts
+++ src/server/router/questions.ts
@@ -7,7 +7,18 @@ export const questionRouter = createRouter()
   .query("get-all", {
     async resolve() {
       return await prisma.pollQuestion.findMany();
+    }}).mutation("create", {
+        input: z.object({
+            question: z.string().min(5).max(600),
+        }),
+        async resolve({input}){
+            const newQuestion = await prisma.pollQuestion.create({
+                data:{
 
-    }})
+                    question:input.question,
+                }
+            })
+        }
+    })
 
 

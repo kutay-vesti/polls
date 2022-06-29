5d91642d06f2ce1958e7ea245a859519d7ac7c4f
Kutay Sakallioglu <kutay@vestiyer.co>
2022-06-29 14:51:09 +0300

    voting working with out ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,64 +4,61 @@ import Link from "next/link";
 import React from "react";
 import { json } from "stream/consumers";
 import { trpc } from "../utils/trpc";
+import { PollQuestion } from "@prisma/client";
+import QuestionCard from "../components/QuestionCard";
 
-const QuestionCreator: React.FC<{}> = () => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const client = trpc.useContext();
-  const { mutate, isLoading } = trpc.useMutation("questions.create", {
-    onSuccess: (data) => {
-      console.log("successs?", data);
-      client.invalidateQueries(["questions.get-all-my-questions"]);
-      if (!inputRef.current) return;
-      inputRef.current.value = "";
-    },
-  });
-  return (
-    <input
-      ref={inputRef}
-      className="border border-gray-300 rounded-lg p-2"
-      disabled={isLoading}
-      onKeyDown={(event) => {
-        if (event.key === "Enter") {
-          console.log("enterr!!", event.currentTarget.value);
-          mutate({ question: event.currentTarget.value });
-        }
-      }}
-    ></input>
-  );
-};
-
-const Home: NextPage = () => {
-  // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-
+export default function Home() {
+  const [showToast, setShowToast] = React.useState(false);
   const { data, isLoading } = trpc.useQuery(["questions.get-all-my-questions"]);
 
-  if (isLoading || !data) return <div>Loading...</div>;
+  const url = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : `http://localhost:${process.env.PORT ?? 3000}`;
+
+  const copyToClipboard = (question: PollQuestion) => {
+    navigator.clipboard.writeText(`${url}/question/${question.id}`);
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 1500);
+  };
+
+  if (isLoading)
+    return (
+      <div className="antialiased min-h-screen flex items-center justify-center">
+        <p className="text-white/40">Loading...</p>
+      </div>
+    );
 
   return (
-    <>
-      <div className="flex flex-col items-center justify-center w-1/2 min-h-screen mx-auto">
-        <h1 className="font-extrabold text-center text-7xl">questions</h1>
-        <QuestionCreator />
-        <div>
-          {data.map((question) => {
-            return (
-              <Link href={`/questions/${question.id}`} key={question.id}>
-                <a>
-                  <div key={question.id}>{question.question}</div>
-                </a>
-              </Link>
-            );
-          })}
-        </div>
+    <div className="p-6 min-h-screen w-screen items-stretch relative">
+      <Head>
+        <title>Home | OnAVote</title>
+      </Head>
+      <header className="header flex w-full justify-between items-center">
+        <h1 className="text-4xl font-bold">OnAVote</h1>
+        <Link href="/create">
+          <a className="bg-gray-300 rounded text-gray-800 p-4">
+            Create New Question
+          </a>
+        </Link>
+      </header>
+      <div className="grid grid-cols-1 gap-y-5 md:grid-cols-4 md:gap-x-5 mt-10">
+        {data?.map((question) => (
+          <QuestionCard
+            key={question.id}
+            question={question}
+            copyToClipboard={copyToClipboard}
+          />
+        ))}
       </div>
-    </>
+
+      {/* Toast that will show at the bottom-right of the screen */}
+      {showToast && (
+        <div className="absolute bottom-5 right-10 flex items-center justify-center bg-slate-50/10 p-3 rounded-md w-1/5">
+          <span className="text-xs font-semibold">
+            Link Copied to Clipboard!
+          </span>
+        </div>
+      )}
+    </div>
   );
-};
-
-export default Home;
-
-// export const getServerSideProps = async () => {
-//   const questions = await prisma.pollQuestion.findMany();
-//   return { props: { questions: JSON.stringify(questions) } };
-// };
+}
